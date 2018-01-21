b3685220ba5fc8d97d7d7e8ca16a1968af9441cd
Gabriel <gabrielbaroni1@gmail.com>
2018-01-21 01:01:33 -0200

    ajustes em busca

--- application/controllers/Produto.php
+++ application/controllers/Produto.php
@@ -5,18 +5,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Produto extends CI_Controller {
 
    public function apiProdutosBusca() {
-      $url = "https://api.github.com/users/" . $this->input->post('login');
+      $url = "https://api.github.com/users/" . $this->input->post('titulo');
       $opts = [
           'http' => [
               'method' => 'GET',
-              'header' => [
-                  'User-Agent: PHP'
+             'header' => [
+                  'User-Agent: PHP',
               ]
           ]
       ];
 
-      $json = json_decode(file_get_contents($url, false, stream_context_create($opts)));
-
+     $json = json_decode(file_get_contents($url, false, stream_context_create($opts)));
+      
       $header['title'] = 'Produto ' . $json->login;
       $header['description'] = 'Confira nossos produtos';
       $this->load->view('header', $header);
